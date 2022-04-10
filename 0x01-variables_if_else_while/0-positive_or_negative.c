269b901dcc8a1b5f30c6faf08017a5b7094c8767
kingsleyabonyi <kinabonyi@gmail.com>
2022-04-10 10:26:44 +0000

    0-positive commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 
         srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -21,5 +21,4 @@ int main(void)
 	else
 		printf("%d is negative\n", n);
 	return (0);
-
 }
