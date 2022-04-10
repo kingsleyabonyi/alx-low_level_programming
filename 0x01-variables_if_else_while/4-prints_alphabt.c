3185865d4aff7e28a23d49043b7bcc41c7ca47d3
kingsleyabonyi <kinabonyi@gmail.com>
2022-04-10 22:09:10 +0000

    4-prints

--- 0x01-variables_if_else_while/4-prints_alphabt.c
+++ 0x01-variables_if_else_while/4-prints_alphabt.c
@@ -13,12 +13,12 @@ int main(void)
 	char low;
 	
 	for (low = 'a'; low <= 'z'; low++)
-	{
+	
 		if (low != e && low != q)
-		{			
+				
 			putchar(low);
-		}			
-	}
+					
+	
 	putchar('\n');
 	return (0);
 }
