593da463af82f0951e6aa357bef65ee5568e1d5a
kingsleyabonyi <kinabonyi@gmail.com>
2022-04-11 14:57:31 +0000

    101-print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-	int d, q, p;
+	int d, p, q;
 
 	for (d = '0'; d < '9'; d++)
 	{
-		for (q = p + 1; q <= '9'; q++)
+		for (p = d + 1; p <= '9'; p++)
 		{
-			for (p = d + 1; p <= '9'; p++)
+			for (q = p + 1; q <= '9'; q++)
 			{
 				if ((q != d) != p)
 				{
@@ -26,7 +26,6 @@ int main(void)
 			
 			if (d == '7' && q == '8')
 				continue;
-
 			putchar(',');
 			putchar(' ');
 				}
