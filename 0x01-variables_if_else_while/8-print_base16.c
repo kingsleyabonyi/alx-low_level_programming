6912724fad3f5d267820e730e4e0d755291bf8bf
kingsleyabonyi <kinabonyi@gmail.com>
2022-04-11 10:48:41 +0000

    8-print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,16 +7,13 @@
  */
 int main(void)
 {
-	char c;
-	
+	int c;
+	char d;
+
 	for (c = '0'; c <= '9'; c++)
-	{
 		putchar(c);
-	}
-	
-	for (c = 'a'; c <= 'f'; c++)
-	{
-		putchar(c);
-	}
+	for (d = 'a'; d <= 'f'; d++)
+		putchar(d);
 	putchar('\n');
 	return (0);
+}
