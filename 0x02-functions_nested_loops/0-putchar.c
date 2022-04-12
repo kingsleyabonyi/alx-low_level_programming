a8dd9100c92711b520464833947abfe41e9f8f71
kingsleyabonyi <kinabonyi@gmail.com>
2022-04-12 11:31:19 +0000

    0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h.>
+#include <main.h>
+
+/**
+ * main - Print putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n")
+	return (0);
+}
+	
