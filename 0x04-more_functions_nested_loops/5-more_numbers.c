022d5ebdac98bbc6dab452143e427f91b5859af3
kingsleyabonyi <kinabonyi@gmail.com>
2022-04-15 05:29:43 +0000

    5 commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,6 +3,7 @@
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
+ * Return: no return
  */
 void more_numbers(void)
 {
@@ -13,8 +14,8 @@ void more_numbers(void)
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-				_putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
+				_putchar((j / 10) + 48);
+			putchar((j % 10 + 48);
 		}
 		_putchar('\n');
 	}
