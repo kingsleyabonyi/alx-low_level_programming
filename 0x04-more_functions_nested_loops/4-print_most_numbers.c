2effe592176a691e0a48074bf68068f305d28f00
kingsleyabonyi <kinabonyi@gmail.com>
2022-04-15 02:22:17 +0000

    4commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,7 +7,7 @@
 void print_most_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
@@ -15,5 +15,6 @@ void print_most_numbers(void)
 			_putchar(i + '0');
 		}
 	}
+
 	_putchar('\n');
 }
