0ce340b8bc8d8690720fa34428df16b01781d637
kingsleyabonyi <kinabonyi@gmail.com>
2022-04-21 17:20:30 +0000

    7-puts commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints half of a string, followed by a new line
+ * @atr: An input string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int let = 0, i, k;
+
+	while (str[let] != '\0')
+		let++;
+	if (let % 2 == 0)
+		i = let / 2;
+	else
+		i = (let + 1) / 2;
+	for (k = i; k < let; k++)
+		_putchar(str[k]);
+	_putchar('\n');
+}
