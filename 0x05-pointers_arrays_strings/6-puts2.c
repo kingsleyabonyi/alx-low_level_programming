3a59d2faf28f7513ba24bf96761c6a414bd5bbcd
kingsleyabonyi <kinabonyi@gmail.com>
2022-04-21 17:01:18 +0000

    6-print

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints next 2 character starting with the first
+ * @str: An input string
+ * Return: void
+ */
+void puts2(char *star)
+{
+	int len = 0, i = 0;
+	while (str[len] != '\0')
+	{
+		if (i & 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		
+		i++;
+	}
+	_putchar('\n');
+}
