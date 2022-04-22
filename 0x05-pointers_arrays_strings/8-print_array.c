52b5e04b1eeec91e5bd21ccd3470138d35807190
kingsleyabonyi <kinabonyi@gmail.com>
2022-04-22 01:48:55 +0000

    to print arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,13 +8,16 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
-
-	for (; i < n; i++)
+	int i =
+	
+	i = 0;
+	for (n--; n >= 0; n--, i++)
 	{
 		printf("%d", a[i]);
-		if (i < n - 1)
+		if (n > 0)
+		{
 			printf(", ");
+		}
 	}
-	putchar('\n');
+	printf("\n");
 }
